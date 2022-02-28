333e641778c2fa26101e4f2d27420055a993061e
parisasledge-source <parisasledge@me.com>
2022-02-27 22:44:22 -0500

    update1

--- src/com/skilldistillery/foodtruck/FoodTruck.java
+++ src/com/skilldistillery/foodtruck/FoodTruck.java
@@ -2,4 +2,63 @@ package com.skilldistillery.foodtruck;
 
 public class FoodTruck {
 
+	  private int truckID;
+	  private String foodName;
+	  private String foodType;
+	  private int truckRating;
+	
+	  public FoodTruck() {
+	  }
+	  
+	  //Create fields for a unique numeric id, a food name , food type, and a numeric rating.
+	  public FoodTruck(int truckID, String foodName, String foodType, int truckRating) {
+	    this.truckID = truckID;
+	    this.foodName = foodName;
+	    this.foodType = foodType;
+	    this.truckRating = truckRating;
+	  }
+	  
+
+	  public int getTruckID() {
+		return truckID;
+	}
+
+	public void setTruckID(int truckID) {
+		this.truckID = truckID;
+	}
+
+	public String getFoodName() {
+		return foodName;
+	}
+
+	public void setFoodName(String foodName) {
+		this.foodName = foodName;
+	}
+
+	public String getFoodType() {
+		return foodType;
+	}
+
+	public void setFoodType(String foodType) {
+		this.foodType = foodType;
+	}
+
+	public int getTruckRating() {
+		return truckRating;
+	}
+
+	public void setTruckRating(int truckRating) {
+		this.truckRating = truckRating;
+	}
+
+	public String toString() {
+		 String output = "Truck ID: " + truckID + ", Food Name: " + foodName + ", Food Type: " + foodType + ", Truck Rating: " + truckRating;
+	    return output;
+	  }
+
+	  public void displayFoodTruck() {
+	    String foodTruckData = this.toString();
+	    System.out.println(foodTruckData);
+	  }
+
 }
--- src/com/skilldistillery/foodtruck/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/FoodTruckApp.java
@@ -1,10 +1,30 @@
 package com.skilldistillery.foodtruck;
 
+
 public class FoodTruckApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+        //Create an array to store FoodTruck objects.
+		FoodTruck foodTruck1 = new FoodTruck(0, "Falafel", "Mediterranean Medic", 3);
 
-	}
+		FoodTruck foodTruck2 = new FoodTruck();
+	    foodTruck2.setTruckID(1);
+	    foodTruck2.setFoodName("Tacos");
+	    foodTruck2.setFoodType("TacoRific");
+	    foodTruck2.setTruckRating(5);
+
+	    //Have the foodTruck display themselves
+	    foodTruck1.displayFoodTruck();
+	    foodTruck2.displayFoodTruck();
+
+	    String foodTruck1Data = foodTruck1.toString();
+	    String foodTruck2Data = foodTruck2.toString();
+
+	    //Get the foodTruck' data and display that
+	    System.out.println("Food Truck 1 data: " + foodTruck1Data);
+	    System.out.println("Food Truck 2 data: " + foodTruck2Data);
+	  }
+
 
 }
